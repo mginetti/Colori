2569fb842a8548edd89dd5f2d367a1066d35dfaf
matteo <matteo.ginetti99@gmail.com>
2019-10-11 14:31:35 +0200

    Base structure html

---
+++ Views/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="topnav">
+        <div class="logo">
+            <img src="../assets/img/logo_min.jpeg" alt="logo" class="img-logo">
+        </div>
+        <div class="link">
+            <a href="./login.php">Login</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="hidden"></div>
+        <div class="band">
+            <img src="../assets/img/img.jpeg" alt="">
+            <div class="band-text">
+                <h1>GAMMA COLORI</h1>
+                <h5>Una gamma dalla infinite possibilità</h5>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+
+</html>
---
+++ Views/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/login.css">
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/9d4635fa48.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="wrapper">
+  <form class="login">
+    <p class="title">Log in</p>
+    <input type="text" placeholder="Username" autofocus/>
+    <i class="fa fa-user"></i>
+    <input type="password" placeholder="Password" />
+    <i class="fa fa-key"></i>
+    <a href="#">Forgot your password?</a>
+    <button>
+      <i class="spinner"></i>
+      <span class="state">Log in</span>
+    </button>
+  </form>
+  </p>
+</div>
+</body>
+</html>
